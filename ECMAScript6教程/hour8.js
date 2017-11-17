bcb3a98470831d03353a7425333af456f6270b7b
once <han-yuantao@haiyisoft.com>
2017-11-17 17:21:32 +0800

    set input

--- ECMAScript6教程/hour8.js
+++ ECMAScript6教程/hour8.js
@@ -8,3 +8,36 @@ let s1 =Symbol('s1')
 console.log(s1)
 
 //Set和Map数据结构
+
+//Set 类似于数组，值是唯一的
+const set = new Set()
+var array = [1,3,2,23,6,20];
+array.forEach(x =>set.add(x));
+console.log(set);
+
+for (let x of set) {
+    console.log(x);
+}
+
+//set 可以通过支持 iterable 的结构作为参数
+const setArray = new Set([1,3,4,5,5,6,6])
+console.error([...setArray])//数组去重
+
+//Set 实例的属性和方法
+// add(value)：添加某个值，返回 Set 结构本身。
+// delete(value)：删除某个值，返回一个布尔值，表示删除是否成功。
+// has(value)：返回一个布尔值，表示该值是否为Set的成员。
+// clear()：清除所有成员，没有返回值。
+var setDemo = new Set();
+setDemo.add('A').add('B').add('C')
+console.log(setDemo)
+console.log(setDemo.size)
+
+console.log(setDemo.has('A'))
+console.log(setDemo.delete('1'))
+setDemo.clear;
+console.log(setDemo)
+
+//遍历操作
+let setFor = new Set(['Ace','Bar','Car','Dog'])
+console.log(setFor.key)
